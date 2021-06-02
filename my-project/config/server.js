758e9ee1bfaa7328ac18a6dde75b93a5b2f008d1
TheCryptoWriter <davidmcneal28@gmail.com>
2021-06-02 10:35:41 -0700

    new commit

--- my-project/config/server.js
+++ my-project/config/server.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
-    host: env('HOST', '0.0.0.0'),
+    host: env('HOST', 'https://tawfiki-design.netlify.app/'),
     prod('')
     port: env.int('PORT', 1337),
     admin: {
